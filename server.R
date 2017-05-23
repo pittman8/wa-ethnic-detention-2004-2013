47a3b5579c89d6fb8973353e8fb115bdb3e5ad01
pittman8 <pittman8@uw.edu>
2017-05-23 14:53:06 -0700

    combined data sets

---
+++ server.R
@@ -0,0 +1,9 @@
+library(shiny)
+library(plotly)
+library(dplyr)
+
+setwd('C:/Users/Hannah/Desktop/INFO-201/wa-ethnic-detention-2004-2013')
+source('./scripts/proportion_detained_from_total.R')
+WA.detention.data <- read.csv('./data/Ethnic_Distribution_of_Detention_Population_2004-2013.csv')
+total.WA.pop <- read.csv('./data/added_column.csv')
+all.data <- full_join(WA.detention.data, total.WA.pop, by = 'Year')
---
+++ ui.R
@@ -0,0 +1,6 @@
+library(shiny)
+library(plotly)
+
+shinyUI(fluidPage(
+  titlePanel("Washington State Ethnic Detention from 2004-2013")))
+
