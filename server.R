518812d34ae1edcf487d0f79c7aee2291cc1c6cb
pittman8 <pittman8@uw.edu>
2017-05-23 15:05:07 -0700

    updated server file

--- server.R
+++ server.R
@@ -7,3 +7,9 @@ source('./scripts/proportion_detained_from_total.R')
 WA.detention.data <- read.csv('./data/Ethnic_Distribution_of_Detention_Population_2004-2013.csv')
 total.WA.pop <- read.csv('./data/added_column.csv')
 all.data <- full_join(WA.detention.data, total.WA.pop, by = 'Year')
+
+shinyServer(function(input, output) {
+  
+  return()
+})
+
